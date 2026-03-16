96409ad53eb2cc9c4edcf1b7d775aa77006e03b4
JaoVicy <filipimjoa@gmail.com>
2026-03-16 20:07:23 -0300

    finish transformPokemon

--- etl-go/internal/service/transform_data.go
+++ etl-go/internal/service/transform_data.go
@@ -10,4 +10,15 @@ func transformPokemon(apiData model.PokemonAPI) model.PokemonDB {
 		bst += s.BaseStat
 	}
 
+	gen := 1
+	if apiData.ID > 151 {
+		gen = 2
+	} // Lógica simplificada para teste
+
+	return model.PokemonDB{
+		ID:         apiData.ID,
+		Name:       apiData.Name,
+		Generation: gen,
+		TotalStats: bst,
+	}
 }
