7e4435cf70e7715509d09a7141ab70cac35263ee
JaoVicy <filipimjoa@gmail.com>
2026-03-16 20:16:50 -0300

    feat: start main func

--- etl-go/cmd/main.go
+++ etl-go/cmd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+// Estruturas para mapear a PokeAPI
+
+func main() {
+	// 1. Get .env configs
+	_ = godotenv.Load()
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not configured")
+	}
+
+	// 2. Connect in Postgres
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, dbURL)
+	if err != nil {
+		log.Fatalf("Error to connect in DB: %v", err)
+	}
+	defer conn.Close(ctx)
+
+	fmt.Println("✅ Connected in Postgres!")
+}
