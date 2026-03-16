a9ba72366ddf72cbd5ed6f5bc4b1b5f7ee670198
JaoVicy <filipimjoa@gmail.com>
2026-03-16 20:31:17 -0300

    build: create dockrfile

--- etl-go/cmd/main.go
+++ etl-go/cmd/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/EGS-software/superset-pipeline/etl-go/internal/model"
+	"github.com/go-resty/resty/v2"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
@@ -29,4 +33,69 @@ func main() {
 	defer conn.Close(ctx)
 
 	fmt.Println("✅ Connected in Postgres!")
+
+	// 3. Criar a tabela se ela não existir (Garante que seu amigo consiga rodar)
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS pokemon_analytics (
+		id INT PRIMARY KEY,
+		name TEXT,
+		generation INT,
+		total_stats INT,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`
+	_, err = conn.Exec(ctx, createTableSQL)
+	if err != nil {
+		log.Fatalf("Erro ao criar tabela: %v", err)
+	}
+
+	// 4. Iniciar Coleta (Exemplo: Primeiros 151 Pokémon - Gen 1)
+	client := resty.New()
+	fmt.Println("🚀 Iniciando extração da PokeAPI...")
+
+	for i := 1; i <= 151; i++ {
+		url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", i)
+		resp, err := client.R().Get(url)
+
+		if err != nil {
+			fmt.Printf("❌ Erro ao buscar ID %d: %v\n", i, err)
+			continue
+		}
+
+		var p model.PokemonAPI
+		json.Unmarshal(resp.Body(), &p)
+
+		// 5. Transformação: Calcular o BST (Base Stat Total)
+		bst := 0
+		for _, s := range p.Stats {
+			bst += s.BaseStat
+		}
+
+		// Lógica simples de geração baseada no ID
+		generation := 1
+		if i > 151 {
+			generation = 2
+		}
+
+		// 6. Salvando no Banco (UPSERT)
+		// O 'ON CONFLICT' garante que se o dado já existir, ele seja atualizado em vez de dar erro
+		upsertSQL := `
+			INSERT INTO pokemon_analytics (id, name, generation, total_stats)
+			VALUES ($1, $2, $3, $4)
+			ON CONFLICT (id) DO UPDATE 
+			SET name = EXCLUDED.name, 
+			    total_stats = EXCLUDED.total_stats,
+			    updated_at = NOW();
+		`
+		_, err = conn.Exec(ctx, upsertSQL, p.ID, p.Name, generation, bst)
+		if err != nil {
+			fmt.Printf("❌ Erro ao salvar %s: %v\n", p.Name, err)
+		} else {
+			fmt.Printf("💾 [%d] %s salvo com BST: %d\n", p.ID, p.Name, bst)
+		}
+
+		// Pequena pausa para não sobrecarregar a API pública
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	fmt.Println("✨ Processo concluído! Dados prontos para análise no Superset.")
 }
