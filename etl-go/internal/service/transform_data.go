aae49f1d6e53121b03c3d823a0954121e5c8e0a1
JaoVicy <filipimjoa@gmail.com>
2026-03-16 20:06:24 -0300

    start transformPokemon

--- etl-go/internal/service/transform_data.go
+++ etl-go/internal/service/transform_data.go
@@ -1,1 +1,13 @@
 package service
+
+import (
+	"github.com/EGS-software/superset-pipeline/etl-go/internal/model"
+)
+
+func transformPokemon(apiData model.PokemonAPI) model.PokemonDB {
+	bst := 0
+	for _, s := range apiData.Stats {
+		bst += s.BaseStat
+	}
+
+}
